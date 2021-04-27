5bcbc06910e8bdf87cfde10298034e4f94d53fae
Tulio Gontijo <tulio_gontijo@hotmail.com>
2021-04-27 10:40:25 -0300

    Ajustes nos patchs do js e JSON

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         let $user = $("#user").val();
         let $pwd = $("#pwd").val();
         if ($user && $pwd) {
-            $.getJSON("../json/users.json", function (registros) {
+            $.getJSON("https://raw.githubusercontent.com/tcgontijo/atv7-pfjs/main/json/users.json", function (registros) {
                 if (registros.usuarios.filter($usuario => $usuario.user == $user && $usuario.pwd == $pwd).length > 0) {
                     alert("Usuário válido!");
                 } else alert("Usuário inválido!")
